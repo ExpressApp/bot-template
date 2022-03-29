76dd9884f83422583215c9f730293d43562d7bf1
Yegor Kutuzov <kutuzov-1313@yandex.ru>
2022-03-29 15:36:23 +0300

    feat: Add callback timeout 30 seconds.

--- {{cookiecutter.bot_project_name}}/app/bot/bot.py
+++ {{cookiecutter.bot_project_name}}/app/bot/bot.py
@@ -6,10 +6,13 @@ from app.bot.commands import common
 from app.bot.error_handlers.internal_error import internal_error_handler
 from app.settings import settings
 
+BOTX_CALLBACK_TIMEOUT = 30
+
 
 def get_bot() -> Bot:
     return Bot(
         collectors=[common.collector],
         bot_accounts=settings.BOT_CREDENTIALS,
         exception_handlers={Exception: internal_error_handler},
+        default_callback_timeout=BOTX_CALLBACK_TIMEOUT,
     )
