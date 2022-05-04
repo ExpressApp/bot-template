96449f1f3d9bf6910b599c9d1424dacb0ff6fb99
Alexander Samoylenko <lxmnk.inbox@gmail.com>
2022-05-04 17:06:17 +0300

    fix: Enable linters for tests

--- {{cookiecutter.bot_project_name}}/tests/test_services/test_botx_user_search.py
+++ {{cookiecutter.bot_project_name}}/tests/test_services/test_botx_user_search.py
@@ -1,10 +1,10 @@
-from uuid import UUID
 from unittest.mock import AsyncMock
+from uuid import UUID
 
 import pytest
 from pybotx import Bot, UserFromSearch, UserNotFoundError
 
-from app.services.botx_user_search import search_user_on_each_cts, UserIsBotError
+from app.services.botx_user_search import UserIsBotError, search_user_on_each_cts
 
 
 async def test_search_user_on_each_cts_user_is_bot_error_raised(
@@ -36,10 +36,12 @@ async def test_search_user_on_each_cts_not_found(
     bot.search_user_by_huid = AsyncMock(side_effect=UserNotFoundError("not found"))
 
     # - Act -
-    found_user = await search_user_on_each_cts(bot, UUID("86c4814b-feee-4ff0-b04d-4b3226318078"))
+    search_result = await search_user_on_each_cts(
+        bot, UUID("86c4814b-feee-4ff0-b04d-4b3226318078")
+    )
 
     # - Assert -
-    assert found_user is None
+    assert search_result is None
 
 
 async def test_search_user_on_each_cts_suceed(
@@ -60,8 +62,13 @@ async def test_search_user_on_each_cts_suceed(
     bot.search_user_by_huid = AsyncMock(return_value=user)
 
     # - Act -
-    found_user, bot_account = await search_user_on_each_cts(bot, UUID("86c4814b-feee-4ff0-b04d-4b3226318078"))
+    search_result = await search_user_on_each_cts(
+        bot, UUID("86c4814b-feee-4ff0-b04d-4b3226318078")
+    )
 
     # - Assert -
+    assert search_result
+
+    found_user, bot_account = search_result
     assert found_user is user
     assert bot_account is list(bot.bot_accounts)[0]
