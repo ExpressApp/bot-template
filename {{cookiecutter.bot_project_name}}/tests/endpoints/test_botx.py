d95e9dfc5e1c31ac3aa2ee81dcdfb784c09d518b
Kiruha01 <liss-2001g@mail.ru>
2022-05-20 14:45:14 +0300

    Add test for unsupported bot api version

--- {{cookiecutter.bot_project_name}}/tests/endpoints/test_botx.py
+++ {{cookiecutter.bot_project_name}}/tests/endpoints/test_botx.py
@@ -248,3 +248,57 @@ def test__web_app__unknown_bot_response_service_unavailable(
 
     # - Assert -
     assert response.status_code == HTTPStatus.SERVICE_UNAVAILABLE
+
+
+@respx.mock
+def test__web_app__unsupported_bot_api_version_service_unavailable(
+    bot: Bot,
+) -> None:
+    # - Arrange -
+    payload = {
+        "bot_id": "c755e147-30a5-45df-b46a-c75aa6089c8f",
+        "command": {
+            "body": "/debug",
+            "command_type": "user",
+            "data": {},
+            "metadata": {},
+        },
+        "entities": [],
+        "file": None,
+        "source_sync_id": None,
+        "sync_id": "6f40a492-4b5f-54f3-87ee-77126d825b51",
+        "from": {
+            "ad_domain": None,
+            "ad_login": None,
+            "app_version": None,
+            "chat_type": "chat",
+            "device": None,
+            "device_meta": {
+                "permissions": None,
+                "pushes": False,
+                "timezone": "Europe/Moscow",
+            },
+            "device_software": None,
+            "group_chat_id": "30dc1980-643a-00ad-37fc-7cc10d74e935",
+            "host": "cts.example.com",
+            "is_admin": True,
+            "is_creator": True,
+            "locale": "en",
+            "manufacturer": None,
+            "platform": None,
+            "platform_package_id": None,
+            "user_huid": "f16cdc5f-6366-5552-9ecd-c36290ab3d11",
+            "username": None,
+        },
+        "proto_version": 3,
+    }
+
+    # - Act -
+    with TestClient(get_application()) as test_client:
+        response = test_client.post(
+            "/command",
+            json=payload,
+        )
+
+    # - Assert -
+    assert response.status_code == HTTPStatus.SERVICE_UNAVAILABLE
