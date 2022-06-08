e2b9f2f9273a3720790a64ce193504df6bd760a7
Arseniy Zhiltsov <arseniy.zhiltsov@ccsteam.ru>
2022-06-08 15:54:54 +0300

    Add ccsteam special files and vars

--- extensions/context.py
+++ extensions/context.py
@@ -6,11 +6,12 @@ from copier.errors import UserMessageError
 
 class ContextUpdater(ContextHook):
     def hook(self, context):
-        try:
-            context["PROD_SERVER_HOST"] = os.environ["PROD_SERVER_HOST"]
-            context["DEV_SERVER_HOST"] = os.environ["DEV_SERVER_HOST"]
-            context["BOTS_REGISTRY_URL"] = os.environ["BOTS_REGISTRY_URL"]
-        except KeyError as exc:
-            raise UserMessageError(f"{exc.args[0]} is not provided in environment")
+        if context["from_ccsteam"]:
+            try:
+                context["PROD_SERVER_HOST"] = os.environ["PROD_SERVER_HOST"]
+                context["DEV_SERVER_HOST"] = os.environ["DEV_SERVER_HOST"]
+                context["BOTS_REGISTRY_URL"] = os.environ["BOTS_REGISTRY_URL"]
+            except KeyError as exc:
+                raise UserMessageError(f"{exc.args[0]} is not provided in environment")
 
         return context
