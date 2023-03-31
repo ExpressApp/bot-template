02e9c1d1baa88c2bedde48e5bdfa44e07e699ff2
Ann S <51851673+Aaaaaaaaaaann@users.noreply.github.com>
2023-03-31 13:49:07 +0300

    refactor: add database constraint naming convention
    refactor: add database constraint naming convention

--- app/db/sqlalchemy.py
+++ app/db/sqlalchemy.py
@@ -3,6 +3,7 @@
 from asyncio import current_task
 from typing import Callable
 
+from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     AsyncSession,
@@ -27,7 +28,15 @@ def make_url_sync(url: str) -> str:
     return "postgresql" + url[url.find(":") :]  # noqa: WPS336
 
 
-Base = declarative_base()
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+Base = declarative_base(metadata=MetaData(naming_convention=convention))
 
 engine: AsyncEngine = create_async_engine(
     make_url_async(settings.POSTGRES_DSN), poolclass=AsyncAdaptedQueuePool
