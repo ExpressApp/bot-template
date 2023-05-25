49eece56c14b4ef2a22660742c7cd4909ae12d08
Nikita Demidovich <nikita.demidovich@ccsteam.ru>
2023-05-25 13:18:50 +0300

    chore(deps): Update dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -119,6 +119,7 @@ def incoming_message_factory(
             metadata={},
             sender=UserSender(
                 huid=user_huid,
+                udid=None,
                 ad_login=ad_login,
                 ad_domain=ad_domain,
                 username=None,
